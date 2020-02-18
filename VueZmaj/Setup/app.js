7428799e02b3ecbb2cc71f2e95530f40a28e04a9
Mateja <mtrikic2@gmail.com>
2020-02-18 16:19:02 +0100

    18.02.2020.

--- VueZmaj/Setup/app.js
+++ VueZmaj/Setup/app.js
@@ -4,54 +4,78 @@ new Vue ({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function() {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function() {
-            this.monsterHealth -= this.calculateDamage(3, 10)
-
-            if (this.monsterHealth <= 0) {
-                alert("YOU WON!");
-                this.gameIsRunning = false;
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'player hits monster = ' + damage
+            });
+            if (this.checkWin()) {
                 return
             }
 
-            this.playerHealth -= this.calculateDamage(5, 12)
-
-            if (this.playerHealth <= 0) {
-                alert("YOU LOST BOI!");
-                this.gameIsRunning = false;
-            }
+            this.monsterAttack()
 
         },
         specialAttack: function() {
+            var damage = this.calculateDamage(10, 20)
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'player hits monster very hard = ' + damage
+            });
+            if (this.checkWin()) {
+                return
+            }
+
+            this.monsterAttack();
 
         },
         heal: function() {
-
+            this.playerHealth += 10;
+            this.monsterAttack();
         },
         giveUp: function() {
+            this.gameIsRunning = false;
 
         },
+        monsterAttack: function() {
+            var damage = this.calculateDamage(5, 13)
+            this.playerHealth -= damage
+            this.checkWin()
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'monster hits player = ' + damage
+            });
+        },
         calculateDamage: function(min, max) {
-            return Math.max(Math.floor(Math.random() * max) + 1, min
+            return Math.max(Math.floor(Math.random() * max) + 1, min)
         },
         checkWin: function() {
             if (this.monsterHealth <= 0) {
-                if (confirm("You Von! New Game?")) {
+                if (confirm("You Won Da Gejm Niga! New Game?")) {
                     this.startGame()
-                } else {
+                }
+                else {
                     this.gameIsRunning = false;
                 }
                 return true;
-            } else if (this.playerHealth <= 0) {
+            }
+            else if (this.playerHealth <= 0) {
                 if (confirm("You lost! New Game?")) {
                     this.startGame()
-                } else {
+                } 
+                else {
                     this.gameIsRunning = false;
                 }
                 return false;
