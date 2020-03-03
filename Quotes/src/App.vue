a2e5812f9e6ad9666892ee2fd729b8f6e50ee7ec
Mateja <mtrikic2@gmail.com>
2020-03-03 17:25:02 +0100

    do 03.03.2020.

---
+++ Quotes/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <app-new-quoute></app-new-quoute>
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    export default {
+        data: function() {
+            return {
+                quotes: [
+                 'jedan'
+                ],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQupte: NewQuote,  
+        }
+    }
+</script>
+
+<style>
+  
+</style>
---
+++ Quotes/src/components/QuoteGrid.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="row">
+         <app-quote :key="quote" v-for="quote in quotes">{{ Quote }}</app-quote>
+    </div>
+</template>
+
+<script>
+    import Quote from './Quote.vue';
+
+    export default {
+        props: ['quotes'],
+        components: {
+            appQuote: Quote
+        }
+    }
+</script>
+
+<style>
+
+</style>
